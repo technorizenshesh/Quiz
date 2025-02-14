b38243599369b04301498dac393bf57c37c8c682
keshav <keshav.technorizen@gmail.com>
2025-02-14 15:45:08 +0530

    update code  14 Feb 2025 By Keshav Savita

--- app/src/main/java/com/smsjuegos/quiz/adapter/HomeAdapter.java
+++ app/src/main/java/com/smsjuegos/quiz/adapter/HomeAdapter.java
@@ -58,9 +58,9 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.SelectTimeView
                 ShowAppLog("onBindViewHolder", eventsListList.get(position).getId(), 1);
                 ShowAppLog("onBindViewHolder--------------", eventsListList.get(position).getCity_id(), 1);
                 if    ( /* eventsListList.get(position).getCity_id().trim().equalsIgnoreCase("3")
-                        |*/
+                        |
                         eventsListList.get(position).getCity_id().trim().equalsIgnoreCase("4")
-                       | eventsListList.get(position).getCity_id().trim().equalsIgnoreCase("5")) {
+                       |*/ eventsListList.get(position).getCity_id().trim().equalsIgnoreCase("5")) {
                     Toast.makeText(context, "Coming Soon..", Toast.LENGTH_LONG).show();
                 } else if (fromHome.equalsIgnoreCase("home"))
                 {
